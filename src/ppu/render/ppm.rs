3541cf6df2d0199122cc3fc6626a4fc923d6f562
Sean Erle Johnson <merehap@protonmail.com>
2026-07-11 10:42:14 -0700

    Tidy PPM parsing

--- src/ppu/render/ppm.rs
+++ src/ppu/render/ppm.rs
@@ -13,27 +13,28 @@ impl Ppm {
         Self { data, pixel_width, pixel_height }
     }
 
-    pub fn from_bytes(mut raw: &[u8]) -> Result<Ppm, String> {
-        let magic_present = parse_exact(b"P6\n", &mut raw) || parse_exact(b"P6\r\n", &mut raw);
+    pub fn from_bytes(raw: &[u8]) -> Result<Ppm, String> {
+        let mut parser = Parser::new(raw);
+        let magic_present = parser.exact(b"P6\n") || parser.exact(b"P6\r\n");
         if !magic_present {
             return Err("PPM magic value missing.".into());
         }
 
-        let pixel_width = parse_number(&mut raw)
+        let pixel_width = parser.number()
             .ok_or("Width missing".to_owned())?;
-        let space_present = parse_exact(b" ", &mut raw);
+        let space_present = parser.exact(b" ");
         if !space_present {
             return Err("Space missing between width and height.".into());
         }
 
-        let pixel_height = parse_number(&mut raw)
+        let pixel_height = parser.number()
             .ok_or("Height missing".to_owned())?;
-        let correct_max_color_value = parse_exact(b"\n255\n", &mut raw) || parse_exact(b"\r\n255\r\n", &mut raw);
+        let correct_max_color_value = parser.exact(b"\n255\n") || parser.exact(b"\r\n255\r\n");
         if !correct_max_color_value {
             return Err("Max color value incorrect or missing.".into());
         }
 
-        let data = raw.to_vec();
+        let data = parser.remainder().to_vec();
         let expected_len = 3 * pixel_width * pixel_height;
         if data.len() != expected_len {
             return Err(format!("Expected PPM data length to be {} but was {}.", expected_len, data.len()));
@@ -59,36 +60,50 @@ impl Ppm {
     }
 }
 
-fn parse_exact(prefix: &[u8], input: &mut &[u8]) -> bool {
-    let stripped = input.strip_prefix(prefix);
-    if let Some(stripped) = input.strip_prefix(prefix) {
-        *input = stripped;
-    }
-
-    stripped.is_some()
+struct Parser<'a> {
+    data: &'a [u8],
 }
 
-fn parse_number(input: &mut &[u8]) -> Option<usize> {
-    let mut output: Option<usize> = None;
-    let mut num_len = 0;
-    for digit in input.iter() {
-        if digit.is_ascii_digit() {
-            let digit = *digit - b'0';
-            if let Some(output) = &mut output {
-                *output = output.checked_mul(10)?;
-                *output = output.checked_add(digit as usize)?;
-            } else {
-                output = Some(digit as usize);
-            }
-        } else {
-            break;
-        }
-
-        num_len += 1;
+impl <'a> Parser<'a> {
+    fn new(data: &'a [u8]) -> Self {
+        Self { data }
     }
 
-    *input = &input[num_len..];
-    output
+    fn exact(&mut self, prefix: &[u8]) -> bool {
+        let stripped = self.data.strip_prefix(prefix);
+        if let Some(stripped) = self.data.strip_prefix(prefix) {
+            self.data = stripped;
+        }
+
+        stripped.is_some()
+    }
+
+    fn number(&mut self) -> Option<usize> {
+        let mut output: Option<usize> = None;
+        let mut num_len = 0;
+        for digit in self.data.iter() {
+            if digit.is_ascii_digit() {
+                let digit = *digit - b'0';
+                if let Some(output) = &mut output {
+                    *output = output.checked_mul(10)?;
+                    *output = output.checked_add(digit as usize)?;
+                } else {
+                    output = Some(digit as usize);
+                }
+            } else {
+                break;
+            }
+
+            num_len += 1;
+        }
+
+        self.data = &self.data[num_len..];
+        output
+    }
+
+    fn remainder(&self) -> &[u8] {
+        self.data
+    }
 }
 
 #[cfg(test)]
@@ -99,51 +114,58 @@ use super::*;
 
     #[test]
     fn exact() {
-        let mut input = &b"P6\n256"[..];
-        assert_eq!(parse_exact(b"P6\n", &mut input), true);
-        assert_eq!(input.len(), 3);
+        let input = &b"P6\n256"[..];
+        let mut parser = Parser::new(input);
+        assert_eq!(parser.exact(b"P6\n"), true);
+        assert_eq!(parser.remainder().len(), 3);
     }
 
     #[test]
     fn exact_failed() {
-        let mut input = &b"P3\n256"[..];
-        assert_eq!(parse_exact(b"P6\n", &mut input), false);
-        assert_eq!(input.len(), 6);
+        let input = &b"P3\n256"[..];
+        let mut parser = Parser::new(input);
+        assert_eq!(parser.exact(b"P6\n"), false);
+        assert_eq!(parser.remainder().len(), 6);
     }
 
     #[test]
     fn number() {
-        let mut input = &b"256abcd"[..];
-        assert_eq!(parse_number(&mut input), Some(256));
-        assert_eq!(input.len(), 4);
+        let input = &b"256abcd"[..];
+        let mut parser = Parser::new(input);
+        assert_eq!(parser.number(), Some(256));
+        assert_eq!(parser.remainder().len(), 4);
     }
 
     #[test]
     fn just_number() {
-        let mut input = &b"240"[..];
-        assert_eq!(parse_number(&mut input), Some(240));
-        assert_eq!(input.len(), 0);
+        let input = &b"240"[..];
+        let mut parser = Parser::new(input);
+        assert_eq!(parser.number(), Some(240));
+        assert_eq!(parser.remainder().len(), 0);
     }
 
     #[test]
     fn bigger_number() {
-        let mut input = &b"293abcd"[..];
-        assert_eq!(parse_number(&mut input), Some(293));
-        assert_eq!(input.len(), 4);
+        let input = &b"293abcd"[..];
+        let mut parser = Parser::new(input);
+        assert_eq!(parser.number(), Some(293));
+        assert_eq!(parser.remainder().len(), 4);
     }
 
     #[test]
     fn empty_number() {
-        let mut input = &b"abcd"[..];
-        assert_eq!(parse_number(&mut input), None);
-        assert_eq!(input.len(), 4);
+        let input = &b"abcd"[..];
+        let mut parser = Parser::new(input);
+        assert_eq!(parser.number(), None);
+        assert_eq!(parser.remainder().len(), 4);
     }
 
     #[test]
     fn number_too_big() {
-        let mut input = &b"123123123123123123123123123123123123123123123123abcd"[..];
-        assert_eq!(parse_number(&mut input), None);
-        assert_eq!(input.len(), 52);
+        let input = &b"123123123123123123123123123123123123123123123123abcd"[..];
+        let mut parser = Parser::new(input);
+        assert_eq!(parser.number(), None);
+        assert_eq!(parser.remainder().len(), 52);
     }
 
     #[test]
